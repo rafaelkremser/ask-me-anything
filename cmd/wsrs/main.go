fa07ffa50c1fb14d1bfa2ffd2a593c53d525fe6b
Rafael Kremser <rafaelfreitaskremser@gmail.com>
2024-08-06 14:58:40 -0300

    feat: create routes

--- cmd/wsrs/main.go
+++ cmd/wsrs/main.go
@@ -24,11 +24,11 @@ func main() {
 
 	pool, err := pgxpool.New(ctx, fmt.Sprintf(
 		"user=%s password=%s host=%s port=%s dbname=%s",
-		os.Getenv("WSRS_DATABASE_USER"),
-		os.Getenv("WSRS_DATABASE_PASSWORD"),
-		os.Getenv("WSRS_DATABASE_HOST"),
-		os.Getenv("WSRS_DATABASE_PORT"),
-		os.Getenv("WSRS_DATABASE_NAME"),
+		os.Getenv("DATABASE_USER"),
+		os.Getenv("DATABASE_PASSWORD"),
+		os.Getenv("DATABASE_HOST"),
+		os.Getenv("DATABASE_PORT"),
+		os.Getenv("DATABASE_NAME"),
 	))
 	if err != nil {
 		panic(err)
